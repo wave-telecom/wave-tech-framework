7dcf16e8d337a28b7030f0bb961521401e34731b
Tatiana Vitorello <vitorellotts@gmail.com>
2025-02-12 13:50:30 -0300

    :test_tube: test: test prisma transaction manager

---
+++ tests/database/transaction.spec.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { PrismaClient } from '@prisma/client';
+import { PrismaTransactionManagerImpl } from 'src/database/transaction';
+import { describe, expect, it } from 'vitest';
+import { mockDeep } from 'vitest-mock-extended';
+
+describe('prisma transaction manager', () => {
+  const prismaClient = mockDeep<PrismaClient>();
+  const sut = new PrismaTransactionManagerImpl(prismaClient);
+
+  it('should call transaction with default values if no values are provided', async () => {
+    prismaClient.$transaction.mockImplementationOnce(async (action: any) => {
+      return Promise.resolve(action(prismaClient));
+    });
+
+    await sut.execute(async (prismaClient: PrismaClient) => {
+      return Promise.resolve(prismaClient.user.findMany());
+    });
+
+    expect(prismaClient.$transaction).toHaveBeenCalledWith(expect.any(Function), {
+      timeout: 30000,
+      isolationLevel: 'ReadCommitted',
+    });
+  });
+
+  it('should execute a transaction', async () => {
+    prismaClient.$transaction.mockImplementationOnce(async (action: any) => {
+      return Promise.resolve(action(prismaClient));
+    });
+
+    prismaClient.user.findMany.mockResolvedValue([]);
+
+    const result = await sut.execute(async (prismaClient: PrismaClient) => {
+      return Promise.resolve(prismaClient.user.findMany());
+    });
+
+    expect(prismaClient.user.findMany).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+});
